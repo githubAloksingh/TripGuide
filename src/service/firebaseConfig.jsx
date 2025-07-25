816fad6d6c615230484495a033a21c9a51aff504
Alok Singh <133560050+githubAloksingh@users.noreply.github.com>
2025-07-25 14:55:38 +0530

    firebaseConfig.jsx

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -18,27 +18,6 @@ const firebaseConfig = {
   measurementId: "G-V4JB0P668S"
 };
 
-
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBh9iDKoIvI3-k5W5kxliEe8F6DNcIlo8Y",
-//   authDomain: "prep-master-e1138.firebaseapp.com",
-//   projectId: "prep-master-e1138",
-//   storageBucket: "prep-master-e1138.firebasestorage.app",
-//   messagingSenderId: "879453703282",
-//   appId: "1:879453703282:web:386b6d6c0ccf7b90ea86eb",
-//   //measurementId: "G-DS3935ZN33"
-// };
-
-
-
-
-
-
-
-
-
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
